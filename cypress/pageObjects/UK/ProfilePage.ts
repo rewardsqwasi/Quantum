c69a3ef79b2fb3a3b11cfd27fc38cfe12495450f
rewardsqwasi <rewards@qwasi.com>
2024-05-20 20:31:17 +0500

    add clickAddUserBtn method

--- cypress/pageObjects/UK/ProfilePage.ts
+++ cypress/pageObjects/UK/ProfilePage.ts
@@ -6,6 +6,11 @@ export default class ProfilePage extends BasePage {
   private section_breadcrumb = '//li[contains(text(),"VELUX Rewards - Edit Profile")]/parent::ul/parent::div[contains(@class,"breadcrumb-section")]';
   private section = '//div[contains(@class,"sign-up-section")]';
   private sectionHeading = '//div[contains(@class,"sign-up-section")]//h4[text()=" Your Profile "]';
+  private add_user_btn = '//span[text()="Add User"]/parent::a';
+
+  clickAddUserBtn(){
+    this.click(this.add_user_btn);
+  }
 
   sectionBreadcrumbElement(){
     return this.element(this.section_breadcrumb);
