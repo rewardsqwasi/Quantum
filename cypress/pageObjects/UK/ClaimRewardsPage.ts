460b057836d68b84df9c66848816ffbddee09b60
rewardsqwasi <rewards@qwasi.com>
2024-06-25 13:50:14 +0500

    add gotoPartners method

--- cypress/pageObjects/UK/ClaimRewardsPage.ts
+++ cypress/pageObjects/UK/ClaimRewardsPage.ts
@@ -8,13 +8,18 @@ export default class ClaimRewardsPage extends BasePage {
   private velux_rewards_shop_div = '//div[contains(@class,"col-span-12")]//a//b[contains(text(),"VELUX Rewards")]';
   private retailers_div = '//div[contains(@class,"col-span-12")]//a//b[text()="Retailers"]';
   private shop_section_div = '.shop-section';
-  
+  private partner_select_btn = '//span[contains(text(),"Select")]/parent::a[contains(@href,"/partner")]';
+
   productSectionDivElement(){
     return this.element(this.product_section_div);
   }
 
   shopSectionDivElement(){
     return this.element(this.shop_section_div);
+  }
+
+  gotoPartners(){
+     this.click(this.partner_select_btn);
   }
 
   viewSection(){
