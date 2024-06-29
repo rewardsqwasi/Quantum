090c770daa65094feacec9e938afc300456db47c
rewardsqwasi <rewards@qwasi.com>
2024-06-29 11:46:21 +0500

    Update login_tests.cy.ts

--- cypress/tests/Regression/FR/login_tests.cy.ts
+++ cypress/tests/Regression/FR/login_tests.cy.ts
@@ -33,7 +33,7 @@ describe('FR Region - Login Tests', () => {
   it('Verify Banners in the header carousel', () => {
     app.loginPage.open(region);
     app.loginPage.bannerDivElement().should('be.visible');
-    app.loginPage.bannerImagesElement().should('have.length', 11);
+    app.loginPage.bannerImagesElement().should('have.length.greaterThan', 10);
   });
 
   it('Verify Footer Links', () => {
