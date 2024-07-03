bf6f5edd69a670c28c451ecf3411d0105a332546
rewardsqwasi <rewards@qwasi.com>
2024-07-03 17:24:22 +0500

    Create VisitsPage.ts

---
+++ cypress/pageObjects/FR/VisitsPage.ts
@@ -0,0 +1,37 @@
+import BasePage from "../BasePage";
+
+export default class VisitsPage extends BasePage {
+
+  private url = '/visits';
+  private section_breadcrumb = '//li[contains(text(),"VELUX & CO - Convertir mes points")]/parent::ul/parent::div[contains(@class,"breadcrumb-section")]';
+  private section_div = '.shop-section';
+  private future_event_heading = '//h1[contains(text(),"Évènements Å Venir")]';
+  private past_event_heading = '//h1[contains(text(),"Évènements Passés")]';
+  private events_div = '//div[contains(@class,"products-1-section")]//div[contains(@class,"grid")]';
+
+  sectionBreadcrumbElement(){
+    return this.element(this.section_breadcrumb);
+  }
+
+  sectionDivElement(){
+    return this.element(this.section_div);
+  }
+
+  futureEventHeadingElement(){
+    return this.element(this.future_event_heading);
+  }
+
+  pastEventHeadingElement(){
+    return this.element(this.past_event_heading);
+  }
+
+  eventsDivElement(){
+    return this.element(this.events_div);
+  }
+
+  open(region: string){
+    this.url = '/'+region+this.url;
+    this.goToUrl(this.url);
+  }
+
+}
