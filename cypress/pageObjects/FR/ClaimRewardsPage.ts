98ff71d00734f7a3ba7070c40498577ce3d77a1e
rewardsqwasi <rewards@qwasi.com>
2024-07-04 13:03:39 +0500

    added locators and methods

--- cypress/pageObjects/FR/ClaimRewardsPage.ts
+++ cypress/pageObjects/FR/ClaimRewardsPage.ts
@@ -12,9 +12,29 @@ export default class ClaimRewardsPage extends BasePage {
   private site_assist_div = '//div[contains(@class,"col-span-12")]//a//b[contains(text(),"Assistance Chantier")]';
   private partners_div = '//div[contains(@class,"col-span-12")]//a//b[contains(text(),"Partenaires")]';
   private partner_select_btn = '//span[contains(text(),"Choisir")]/parent::a[contains(@href,"/partner")]';
+  private presentoirs_select_btn = '//span[contains(text(),"Choisir")]/parent::a[contains(@href,"/presentoirs")]';
+  private visits_select_btn = '//span[contains(text(),"Choisir")]/parent::a[contains(@href,"/visit")]';
+  private tickets_select_btn = '//span[contains(text(),"Choisir")]/parent::a[contains(@href,"/tickets")]';
+  private my_project_select_btn = '//span[contains(text(),"Choisir")]/parent::a[contains(@href,"/mon-projet")]';
+
+  gotoMyProject(){
+    this.click(this.my_project_select_btn);
+  }
+
+  gotoTickets(){
+    this.click(this.tickets_select_btn);
+  }
+
+  gotoVisits(){
+    this.click(this.visits_select_btn);
+  }
 
   gotoPartners(){
     this.click(this.partner_select_btn);
+  }
+
+  gotoPresentoirs(){
+    this.click(this.presentoirs_select_btn);
   }
 
   partnersDivElement(){
