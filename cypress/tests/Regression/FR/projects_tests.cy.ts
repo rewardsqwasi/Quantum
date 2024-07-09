e5dfaab2f27ab25be393e84a48a88528bf0e60e1
rewardsqwasi <rewards@qwasi.com>
2024-07-09 14:17:42 +0500

    Create projects_tests.cy.ts

---
+++ cypress/tests/Regression/FR/projects_tests.cy.ts
@@ -0,0 +1,43 @@
+import App from 'cypress/pageObjects/FR/AppPage';
+
+const IS_PROD = Cypress.env('IS_PROD');
+const member = {
+  "email": Cypress.env('MEMBER_EMAIL_FR'),
+  "password": Cypress.env('MEMBER_PASSWORD_FR')
+};
+let app = new App();
+let region = 'fr';
+
+describe('FR Region - Projects Tests', () => {
+
+  before(() => {
+    app.loginPage.open(region);
+    if(IS_PROD==="true"){
+      app.loginPage.allowCookie();
+      //app.getURL().should('contain', '?consent=preferences,statistics,marketing&ref-original=');
+    }
+    app.loginPage.open(region);
+    app.loginPage.login(member.email, member.password);
+  });
+
+  it('Verify Page URL', () => {
+    app.homePage.clickProjectsBtn();
+    let url = Cypress.env('BASE_URL') + '/'+region+'/projects';
+    app.getURL().should('contain', url);    
+  });
+
+  it('Verify Page Title', () => {
+    app.getPageTitle().should('eq','VELUX & CO - Mon Projet');
+  });
+
+  it('Verify Section', () => {
+    app.projectPage.sectionElement().should('be.visible');
+    app.projectPage.sectionBreadcrumbElement().should('be.visible');
+    app.projectPage.tableElement().should('be.visible');
+    app.projectPage.tableHeadingElement().should('be.visible');
+    app.projectPage.tableHeaderColumnElement().should('be.visible');
+    app.projectPage.filterStatusSelectElement().should('be.visible');
+    app.projectPage.paginationSelectElement().should('be.visible');
+  });
+
+})
