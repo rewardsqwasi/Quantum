93fd8bca4c4c4b0f9c9541f763993aa49aa4f032
rewardsqwasi <rewards@qwasi.com>
2024-07-26 17:49:32 +0500

    added new locators and its method

--- cypress/pageObjects/UK/ShopPage.ts
+++ cypress/pageObjects/UK/ShopPage.ts
@@ -7,17 +7,51 @@ export default class ShopPage extends BasePage {
   private section_breadcrumb = '//li[contains(text(),"VELUX Rewards - Shop")]/parent::ul/parent::div[contains(@class,"breadcrumb-section")]';
   private section = '//div[contains(@class,"shop-section")]';
   private product_section = '//div[contains(@class,"products")]';
-  private product_section_heading = '//div[contains(@class,"products")]//h1[text()="All Products"]';
+  private product_section_heading = '//div[contains(@class,"products")]//h1[text()="All Products" or text()=" Products "]';
   private product_div = '//div[contains(@class,"products")]/div[@id="GridView"]';
   private search_filter_form = '//h1[text()="Search Filter"]/parent::div/parent::form';
   private search_filter_checkboxes = '//div[@class="see-more-content"]';
-  private filter_button = '//button/span[text()="Filter"]';
-  private reset_button = '//button/span[text()="Reset"]';
+  private filter_button = '//button/span[contains(text(),"Filter")]';
+  private reset_button = '//button/span[contains(text(),"Reset")]';
   private first_product_link = '((//div[@id="GridView"]//div[contains(@class,"card-container")])[1]//a)[1]';
   private product_name = '//div[contains(@class,"product-details")]//h2';
   private product_code = '//div[contains(@class,"product-details")]//h3[1]';
   private product_points = '//div[contains(@class,"product-details")]//span[contains(text(),"Points:")]';
   private add_to_basket_btn = '//span[contains(text(),"Add to basket")]/parent::button';
+  private product_code_2 = '//div[contains(@class,"product-details")]//span[text()="Product Number: "]/following-sibling::b';
+  private product_points_2 = '//div[contains(@class,"product-details")]//span[contains(text(),"Points")]';
+  private add_to_basket_btn_2 = '//span[text()="Add"]/parent::a';
+  private success_toast = '//div[@class="toast-message" and text()="This product was added to your cart successfully"]';
+  
+  successToastElement(){
+    return this.element(this.success_toast);
+  }
+
+  clickAddToBasket2(){
+    this.click2(this.add_to_basket_btn_2);
+  }
+
+  getProductDetail2(){
+    let productDetails = {
+      name: '',
+      code: '',
+      points: ''
+    };
+
+    return this.element(this.product_name).then($el => {
+      productDetails.name = $el.text().trim();
+      return this.element(this.product_code_2);
+    })
+    .then($el => {
+      productDetails.code = $el.text().trim();
+      return this.element(this.product_points_2);
+    })
+    .then($el => {
+      productDetails.points = $el.text().replace("Reward Points:", "").trim();
+      return this.wrap([productDetails.name, productDetails.points, productDetails.code]);
+    });
+
+  }
 
   clickFirstProduct(){
     this.click(this.first_product_link);
