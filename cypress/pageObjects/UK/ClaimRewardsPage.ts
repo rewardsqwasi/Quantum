d7db13beb876ce295f9f486f31ab03d597df57a1
rewardsqwasi <rewards@qwasi.com>
2024-08-01 17:47:26 +0500

    added velux_rewards_shop_div2 locator and method

--- cypress/pageObjects/UK/ClaimRewardsPage.ts
+++ cypress/pageObjects/UK/ClaimRewardsPage.ts
@@ -6,6 +6,7 @@ export default class ClaimRewardsPage extends BasePage {
   private product_section_div = '//h1[text()="VELUX & Me"]//ancestor::div[@id="product_list_view"]';
   private heading = '//div[@id="product_list_view"]//h1[text()="VELUX & Me"]';
   private velux_rewards_shop_div = '//div[contains(@class,"col-span-12")]//a//b[contains(text(),"VELUX Rewards")]';
+  private velux_rewards_shop_div2 = '//div[contains(@class,"col-span-12")]//a//b/p[contains(text(),"VELUX Rewards")]';
   private retailers_div = '//div[contains(@class,"col-span-12")]//a//b[text()="Retailers"]';
   private shop_section_div = '.shop-section';
   private partner_select_btn = '//span[contains(text(),"Select")]/parent::a[contains(@href,"/partner")]';
@@ -32,6 +33,10 @@ export default class ClaimRewardsPage extends BasePage {
 
   veluxRewardsShopDivElement(){
     return this.element(this.velux_rewards_shop_div);
+  }
+
+  veluxRewardsShopDiv2Element(){
+    return this.element(this.velux_rewards_shop_div2);
   }
 
   retailersDivElement(){
