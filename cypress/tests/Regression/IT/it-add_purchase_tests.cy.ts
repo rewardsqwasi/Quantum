b4868b6e9e5b3a8c9a5478b01a2eeed4356f1b67
rewardsqwasi <rewards@qwasi.com>
2024-09-02 18:11:15 +0500

    Created Add Purchase test

---
+++ cypress/tests/Regression/IT/it-add_purchase_tests.cy.ts
@@ -0,0 +1,107 @@
+import { Helper } from 'cypress/util/helper';
+import App from 'cypress/pageObjects/IT/AppPage';
+
+const member = {
+  "email": Cypress.env('MEMBER_EMAIL_IT'),
+  "password": Cypress.env('MEMBER_PASSWORD_IT')
+};
+let app = new App();
+let region = 'it';
+
+describe('IT Region - Add Purchase Tests', () => {
+
+  before(() => {
+    app.loginPage.allowCookie();
+    app.loginPage.open(region);
+    app.loginPage.login(member.email, member.password);
+  });
+
+  it('Verify Page URL', () => {
+    app.homePage.clickAddPurchseBtn();
+    let addPurUrl = Cypress.env('BASE_URL') + '/'+region+'/add-purchase';
+    app.getURL().should('contain', addPurUrl);
+  });
+
+  it('Verify Page Title', () => {
+    app.getPageTitle().should('eq','VELUX Premia - Aggiungi certificato');
+  });
+
+  it('Verify Add Purchase Section', () => {
+    app.addPurchasePage.sectionBreadcrumbElement().should('be.visible');
+    app.addPurchasePage.addPurchaseSectionElement().should('be.visible');
+    app.addPurchasePage.viewSection();
+    app.addPurchasePage.headingElement().should('be.visible');
+    app.addPurchasePage.headlineTextElement().should('be.visible');
+    app.addPurchasePage.formHeadingElement().should('be.visible');
+    app.addPurchasePage.formSectionElement().should('be.visible');
+    app.addPurchasePage.cancelBtnElement().should('be.visible');
+    app.addPurchasePage.continueBtnElement().should('be.visible');
+  });
+/*
+  it('Verify that the number dropdown is displayed and contains the correct values from 1 to 201.', () => {
+    app.addPurchasePage.selectHowManyPropertyDropdown().find('option').should('have.length', 202)
+    .each((option, index) => {
+      if (index === 0) {
+        cy.wrap(option).should('have.text', 'Vänligen skriv in ett nummer');
+      }
+      else {
+        const expectedValue = index.toString();
+        app.wrap(option).should('have.value', expectedValue);
+        app.wrap(option).should('have.text', expectedValue);
+      }
+    });
+  });
+
+  it('Verify that submitting the form with invalid data displays appropriate error message.', () => {
+    app.addPurchasePage.clickContinueBtn();
+    app.addPurchasePage.selectPropertyValidationMsg().should('eq', "Please select an item in the list.");
+  });
+*/
+  it('Verify 1 Property & Dont Know Installation Invoice Purchase', () => {
+    //app.addPurchasePage.selectHowManyProperty('1');
+    app.addPurchasePage.describeInstallationSectionElement().should('be.visible');
+    app.addPurchasePage.selectDescribeInstallation('Non saprei');
+    app.addPurchasePage.clickContinueBtn();
+    app.addPurchasePage.uploadInvoiceFormSectionElement().should('be.visible');
+    app.addPurchasePage.viewUploadInvoiceSection();
+    app.addPurchasePage.uploadInvoice('testInvoice.pdf').should('be.visible');
+    //app.addPurchasePage.selectAddPurchaseCheckBox();
+    app.addPurchasePage.clickFinishBtn();
+    //app.addPurchasePage.successPurchaseToastElement().should('be.visible');
+    app.addPurchasePage.getHeadingText().should('contain', "Grazie per aver caricato il certificato!");
+    app.homePage.clickPointsHistoryBtn();
+    app.purchaseStatusPage.dateOfPurchase().should('be.oneOf', Helper.currentDate());
+    app.purchaseStatusPage.refOfPurchase().should('match',/^\n\d+\n$/);
+    app.purchaseStatusPage.statusOfPurchase().should('eq', 'In sospeso');
+  });
+
+  it('Verify 1 Property, Replacing existing roof windows and Hall room Installation Invoice Purchase', () => {
+    app.homePage.clickAddPurchseBtn();
+    //app.addPurchasePage.selectHowManyProperty('1');
+    app.addPurchasePage.describeInstallationSectionElement().should('be.visible');
+    app.addPurchasePage.selectDescribeInstallation('Sostituzione di finestra/e esistente/i');
+    app.addPurchasePage.inWhichRoomInstallationSectionElement().should('be.visible');
+    app.addPurchasePage.selectWhichRoomInstallation('Sala');
+    app.addPurchasePage.enterPostalCode('32165');
+    app.addPurchasePage.clickContinueBtn();
+    app.addPurchasePage.uploadInvoiceFormSectionElement().should('be.visible');
+    app.addPurchasePage.viewUploadInvoiceSection();
+    app.addPurchasePage.uploadInvoice('testInvoice.pdf').should('be.visible');
+    //app.addPurchasePage.selectAddPurchaseCheckBox();
+    app.addPurchasePage.clickFinishBtn();
+    //app.addPurchasePage.successPurchaseToastElement().should('be.visible');
+    app.addPurchasePage.getHeadingText().should('contain', "Grazie per aver caricato il certificato!");
+    app.homePage.clickPointsHistoryBtn(); 
+    app.purchaseStatusPage.dateOfPurchase().should('be.oneOf', Helper.currentDate());
+    app.purchaseStatusPage.refOfPurchase().should('match',/^\n\d+\n$/);
+    app.purchaseStatusPage.statusOfPurchase().should('eq', 'In sospeso');
+  });
+
+  it('Verify “Cancel” button is clickable and redirects to the home page.', () => {
+    app.homePage.clickAddPurchseBtn();
+    app.addPurchasePage.clickCancelBtn();
+    let url = Cypress.env('BASE_URL') + '/'+region+'/home';
+    app.getURL().should('contain', url);
+  });
+
+})
