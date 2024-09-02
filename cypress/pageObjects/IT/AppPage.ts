1774476b603ceb6d05b5c8f10cd7596aa63980b5
rewardsqwasi <rewards@qwasi.com>
2024-09-02 18:03:03 +0500

    Encapsulate more page object files

--- cypress/pageObjects/IT/AppPage.ts
+++ cypress/pageObjects/IT/AppPage.ts
@@ -6,11 +6,18 @@ import HomePage from './HomePage';
 import ForgetPinPage from './ForgetPinPage';
 import EmailPage from './EmailPage';
 import AddPurchasePage from './AddPurchasePage';
-import DSPPage from './DSPPage';
-import AddReplacementPage from './AddReplacementPage';
 import PurchaseStatusPage from './PurchaseStatusPage';
-import TrainingPointsPage from './TrainingPointsPage';
 import ClaimRewardsPage from './ClaimRewardsPage';
+import PartnersPage from './PartnersPage';
+import RewardsCalculatorPage from './RewardsCalculatorPage';
+import DashboardPage from './DashboardPage';
+import RewardsHistoryPage from './RewardsHistoryPage';
+import ProfilePage from './ProfilePage';
+import ContactUsPage from './ContactUsPage';
+import TermsPage from './TermsPage';
+import BasketPage from './BasketPage';
+import ShopPage from './ShopPage';
+import AddUserPage from './AddUserPage';
 
 export default class App extends BasePage {
    
@@ -38,26 +45,52 @@ export default class App extends BasePage {
         return new AddPurchasePage();
     }
 
-    public get dspPage(): DSPPage {
-        return new DSPPage();
-    }
-
-    public get addReplacementPage(): AddReplacementPage {
-        return new AddReplacementPage();
-    }
-
     public get purchaseStatusPage(): PurchaseStatusPage {
         return new PurchaseStatusPage();
-    }
-
-    public get trainingPointsPage(): TrainingPointsPage {
-        return new TrainingPointsPage();
     }
 
     public get claimRewardsPage(): ClaimRewardsPage {
         return new ClaimRewardsPage();
     }
 
-    
-    
+    public get partnersPage(): PartnersPage {
+        return new PartnersPage();
+    }
+
+    public get rewardsCalculatorPage(): RewardsCalculatorPage {
+        return new RewardsCalculatorPage();
+    }
+
+    public get dashboardPage(): DashboardPage {
+        return new DashboardPage();
+    }
+
+    public get rewardsHistoryPage(): RewardsHistoryPage {
+        return new RewardsHistoryPage();
+    }
+
+    public get profilePage(): ProfilePage {
+        return new ProfilePage();
+    }
+
+    public get contactUsPage(): ContactUsPage {
+        return new ContactUsPage();
+    }
+
+    public get termsPage(): TermsPage {
+        return new TermsPage();
+    }
+
+    public get basketPage(): BasketPage {
+        return new BasketPage();
+    }
+
+    public get shopPage(): ShopPage {
+        return new ShopPage();
+    }
+
+    public get addUserPage(): AddUserPage {
+        return new AddUserPage();
+    }
+
 }
