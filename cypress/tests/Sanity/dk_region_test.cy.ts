0e6b11389bb9f0b5c0dd77fc813eae45066367e6
rewardsqwasi <rewards@qwasi.com>
2024-09-09 19:48:41 +0500

    fix test

--- cypress/tests/Sanity/dk_region_test.cy.ts
+++ cypress/tests/Sanity/dk_region_test.cy.ts
@@ -52,7 +52,7 @@ describe('Sanity Test Denmark Region', () => {
     //app.getURL().should('contain', eLearningUrl);
     //app.getPageTitle().should('eq','VELUX Rewards - E-learning');
     
-    app.homePage.clickSpendPointsBtn();
+    app.homePage.clickClaimRewardsBtn();
     let claimRewardUrl = Cypress.env('BASE_URL') + '/'+region+'/claim-rewards';
     app.getURL().should('contain', claimRewardUrl);
     app.getPageTitle().should('eq','VELUX håndværkerbonus - Anvend bonuskroner');
