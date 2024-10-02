698f03e3a3d20127c318e8018b91d46dda270754
rewardsqwasi <rewards@qwasi.com>
2024-10-03 01:21:16 +0500

    update locators and their methods

--- cypress/pageObjects/CH/ClaimRewardsPage.ts
+++ cypress/pageObjects/CH/ClaimRewardsPage.ts
@@ -3,29 +3,115 @@ import BasePage from "../BasePage";
 export default class ClaimRewardsPage extends BasePage {
 
   private url = '/claim-rewards';
-  private product_section_div = '//h1[text()="VELUX & Me"]//ancestor::div[@id="product_list_view"]';
-  private heading = '//div[@id="product_list_view"]//h1[text()="VELUX & Me"]';
-  private velux_rewards_shop_div = '//div[contains(@class,"col-span-12")]//a//b[contains(text(),"VELUX Rewards")]';
-  private retailers_div = '//div[contains(@class,"col-span-12")]//a//b[text()="Retailers"]';
+  private section = '//div[@id="content"]/section';
+  private section_heading = '//section//h2[text()="VELUX & Ich"]';
+  private section_heading_2 = '//section//h2[text()="Prämienpartner"]';
+  private dealer_select_btn = '//a[text()="Auswählen" and contains(@href,"/dealers")]';
+  private dealer_div = '//div[@class="row"]//h3/a[text()="Finden Sie Ihren Lieblings-Händler"]';
+  private zalando_select_btn = '//a[text()="Auswählen" and contains(@href,"/partners/21")]';
+  private zalando_div = '//div[@class="row"]//h3/a[text()="Zalando"]';
+  private ticketcorner_select_btn = '//a[text()="Auswählen" and contains(@href,"/partners/22")]';
+  private ticketcorner_div = '//div[@class="row"]//h3/a[text()="Ticketcorner"]';
+  private media_select_btn = '//a[text()="Auswählen" and contains(@href,"/partners/23")]';
+  private media_div = '//div[@class="row"]//h3/a[text()="Media Markt"]';
+  private manor_select_btn = '//a[text()="Auswählen" and contains(@href,"/partners/24")]';
+  private manor_div = '//div[@class="row"]//h3/a[text()="Manor"]';
+  private coop_select_btn = '//a[text()="Auswählen" and contains(@href,"/partners/25")]';
+  private coop_div = '//div[@class="row"]//h3/a[text()="Coop"]';
+  private bonus_detail_heading = '//h1[text()="Details zu Ihrer Auswahl"]';
+  private reward_detail_element = '//div[@id="reward-details"]';
+  private cancel_btn = '//a[text()="Vorgang abbrechen"]';
+  private sidebar_div = '//div[@id="sidebar"]';
+  private claim_reward_btn = '//div[@id="point-details"]/a[text()="Punkte einlösen"]';
+  private my_vouchers_btn = '//div[@id="point-details"]/a[text()="Meine Gutscheine"]';
+  private all_vouchers_btn = '//div[@id="point-details"]/a[text()="Alle Gutscheine"]';
+
+  allVouchersBtnElement(){
+    return this.element(this.all_vouchers_btn);
+  }
+
+  myVouchersBtnElement(){
+    return this.element(this.my_vouchers_btn);
+  }
+
+  claimRewardBtnElement(){
+    return this.element(this.claim_reward_btn);
+  }
+
+  sidebarDivElement(){
+    return this.element(this.sidebar_div);
+  }
+
+  clickCancelBtn(){
+    this.click(this.cancel_btn);
+  }
+
+  rewardDetailElement(){
+    return this.element(this.reward_detail_element);
+  }
+
+  bonusDetailHeadingElement(){
+    return this.element(this.bonus_detail_heading);
+  }
+
+  sectionHeading2Element(){
+    return this.element(this.section_heading_2);
+  }
+
+  dealerDivElement(){
+    return this.element(this.dealer_div);
+  }
+
+  gotoDealer(){
+    this.click(this.dealer_select_btn);
+  }
+
+  coopDivElement(){
+    return this.element(this.coop_div);
+  }
+
+  gotoCoop(){
+    this.click(this.coop_select_btn);
+  }
+
+  manorDivElement(){
+    return this.element(this.manor_div);
+  }
+
+  gotoManor(){
+    this.click(this.manor_select_btn);
+  }
+
+  mediaDivElement(){
+    return this.element(this.media_div);
+  }
+
+  gotoMedia(){
+    this.click(this.media_select_btn);
+  }
+
+  zalandoDivElement(){
+    return this.element(this.zalando_div);
+  }
+
+  gotoZalando(){
+    this.click(this.zalando_select_btn);
+  }
+
+  sectionHeadingElement(){
+    return this.element(this.section_heading);
+  }
+
+  sectionElement(){
+    return this.element(this.section);
+  }
   
-  productSectionDivElement(){
-    return this.element(this.product_section_div);
+  ticketcornerDivElement(){
+    return this.element(this.ticketcorner_div);
   }
 
-  viewSection(){
-    this.inView(this.product_section_div);
-  }
-
-  headingElement(){
-    return this.element(this.heading);
-  }
-
-  veluxRewardsShopDivElement(){
-    return this.element(this.velux_rewards_shop_div);
-  }
-
-  retailersDivElement(){
-    return this.element(this.retailers_div);
+  gotoTicketcorner(){
+    this.click(this.ticketcorner_select_btn);
   }
 
   open(region: string){
