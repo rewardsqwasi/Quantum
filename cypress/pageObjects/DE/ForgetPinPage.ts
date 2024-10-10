f2a50f6b5f48427782a2e08910d41d8ec95f8c56
rewardsqwasi <rewards@qwasi.com>
2024-10-10 06:31:24 +0500

    update locators and methods

--- cypress/pageObjects/DE/ForgetPinPage.ts
+++ cypress/pageObjects/DE/ForgetPinPage.ts
@@ -5,16 +5,34 @@ export default class ForgetPinPage extends BasePage {
   private url = '/forget-pin';
   private captcha_div = '.g-recaptcha';
   private captcha_error_div = '#g-recaptcha-error';
-  private reset_password_heading = '//h1[text()="Reset Password"]';
+  private reset_password_heading = '//h1[text()="Passwort vergessen?"]';
   private email_field = '#email';
-  private info_text_one = '//p[text()="You will shortly receive an email and SMS detailing instructions on how to reset your password."]';
-  private info_text_two = '//p[text()="The SMS will also contain a verification code."]';
+  private info_text_one = '//p[text()="Ihre Zugangsdaten werden an Ihre E-Mailadresse gesendet. Bitte prüfen Sie Ihren SPAM-Ordner, sollten Sie die E-Mail nicht im Posteingang erhalten. Sie haben eine Stunde nach Erhalt der Email Zeit, Ihr Passwort zu ändern."]';
   private request_new_pass_btn = '#submit';
   private toastMessage = '.toast-message';
+  private enter_email_error = '//span[@id="emailStatus" and text()="Bitte geben Sie Ihre E-Mailadresse ein."]';
+  private enter_email_error_2 = '//span[@id="emailStatus" and text()="Bitte überprüfen Sie Ihre E-Mailadresse."]';
+  private re_captcha_error = '//div[@id="g-recaptcha-error" and ./span[contains(text(),"reCaptcha ist erforderlich")]]';
+
+  enterEmailErrorElement(){
+    return this.element(this.enter_email_error);
+  }
+
+  enterEmailError2Element(){
+    return this.element(this.enter_email_error_2);
+  }
+
+  reCaptchaErrorElement(){
+    return this.element(this.re_captcha_error);
+  }
 
   removeCaptchaDiv(){
     this.removeFromDom(this.captcha_div);
     this.removeFromDom(this.captcha_error_div);
+  }
+
+  captchaDivElement(){
+    return this.element(this.captcha_div);
   }
 
   resetPasswordHeadingElement(){
@@ -23,10 +41,6 @@ export default class ForgetPinPage extends BasePage {
 
   infoTextOneElement(){
     return this.element(this.info_text_one);
-  }
-
-  infoTextTwoElement(){
-    return this.element(this.info_text_two);
   }
 
   viewResetPasswordHeading(){
@@ -40,9 +54,14 @@ export default class ForgetPinPage extends BasePage {
   requestPassBtnElement(){
     return this.element(this.request_new_pass_btn);
   }
+  
+  reqNewPassword(email: string){
+    this.removeCaptchaDiv();
+    this.type(this.email_field, email);
+    this.click(this.request_new_pass_btn);
+  }
 
   enterEmail(email: string){
-    this.removeCaptchaDiv();
     this.type(this.email_field, email);
   }
 
