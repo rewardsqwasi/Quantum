4be3f91480a4cd496cc87c3dfb34dfccde5747a0
rewardsqwasi <rewards@qwasi.com>
2024-10-10 06:32:18 +0500

    update AppPage object file of DE region

--- cypress/pageObjects/DE/AppPage.ts
+++ cypress/pageObjects/DE/AppPage.ts
@@ -4,13 +4,12 @@ import LoginPage from './LoginPage';
 import CreateAccountPage from './CreateAccountPage';
 import HomePage from './HomePage';
 import ForgetPinPage from './ForgetPinPage';
-import EmailPage from './EmailPage';
 import AddPurchasePage from './AddPurchasePage';
-import DSPPage from './DSPPage';
-import AddReplacementPage from './AddReplacementPage';
 import PurchaseStatusPage from './PurchaseStatusPage';
-import TrainingPointsPage from './TrainingPointsPage';
-import ClaimRewardsPage from './ClaimRewardsPage';
+import ProfilePage from './ProfilePage';
+import DashboardPage from './DashboardPage';
+import ContactUsPage from './ContactUsPage';
+import TermsPage from './TermsPage';
 
 export default class App extends BasePage {
    
@@ -30,34 +29,28 @@ export default class App extends BasePage {
         return new ForgetPinPage();
     }
 
-    public get emailPage(): EmailPage {
-        return new EmailPage();
-    }
-
     public get addPurchasePage(): AddPurchasePage {
         return new AddPurchasePage();
     }
 
-    public get dspPage(): DSPPage {
-        return new DSPPage();
-    }
-
-    public get addReplacementPage(): AddReplacementPage {
-        return new AddReplacementPage();
-    }
-
     public get purchaseStatusPage(): PurchaseStatusPage {
         return new PurchaseStatusPage();
-    }
-
-    public get trainingPointsPage(): TrainingPointsPage {
-        return new TrainingPointsPage();
-    }
-
-    public get claimRewardsPage(): ClaimRewardsPage {
-        return new ClaimRewardsPage();
-    }
-
+    }  
     
+    public get profilePage(): ProfilePage {
+        return new ProfilePage();
+    }
+
+    public get dashboardPage(): DashboardPage {
+        return new DashboardPage();
+    }
+
+    public get contactUsPage(): ContactUsPage {
+        return new ContactUsPage();
+    }
+
+    public get termsPage(): TermsPage {
+        return new TermsPage();
+    }
     
 }
